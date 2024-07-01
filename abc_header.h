c56795c94e90ac395036efa1b3e98ad398e035c7
SergeyKharenko <u201810610@hust.edu.cn>
2024-07-01 16:42:25 +0800

    add abc_header.h in convenient for outside usage

---
+++ abc_header.h
@@ -0,0 +1,15 @@
+//
+// Created by kharenko on 7/1/24.
+//
+
+#ifndef ABC_HEADER_H
+#define ABC_HEADER_H
+
+
+#define ABC_NAMESPACE             abc
+#define ABC_USE_PTHREADS
+#define ABC_USE_READLINE
+#define ABC_USE_CUDD            1
+#define ABC_USE_STDINT_H        1
+
+#endif //ABC_HEADER_H
