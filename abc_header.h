8dbca1bc38c1049cddbba96c12518c065b962a4d
SergeyKharenko <u201810610@hust.edu.cn>
2024-07-01 17:20:13 +0800

    add dependency in abc_header.h

--- abc_header.h
+++ abc_header.h
@@ -5,11 +5,13 @@
 #ifndef ABC_HEADER_H
 #define ABC_HEADER_H
 
-
 #define ABC_NAMESPACE             abc
 #define ABC_USE_PTHREADS
 #define ABC_USE_READLINE
 #define ABC_USE_CUDD            1
 #define ABC_USE_STDINT_H        1
+
+#include "src/misc/util/abc_global.h"
+#include "daemon/abcapis.h"
 
 #endif //ABC_HEADER_H
